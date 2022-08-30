a22c152ca5a8e64f13cbed2913244bc5b1e48c47
bark20 <109058939+bark20@users.noreply.github.com>
2022-08-30 15:35:40 +0900

    Revert "#[5] feat: 기본 프로젝트 생성, 에러코드 기능 구현"

--- yoseobara/src/main/java/com/final2/yoseobara/exception/InvalidValueException.java
+++
@@ -1,8 +0,0 @@
-package com.final2.yoseobara.exception;
-
-/* 잘못된 값이 사용되었을 때 사용*/
-public class InvalidValueException extends BusinessException {
-    public InvalidValueException(ErrorCode errorCode) {
-        super(errorCode);
-    }
-}
