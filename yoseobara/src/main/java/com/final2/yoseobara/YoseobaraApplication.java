7ff33fc468552c754c09f3d6437320550252b796
siwonkim <ktldnjs0811@naver.com>
2022-08-30 11:40:41 +0900

    #[5] feat: 기본 프로젝트 생성, 에러코드 기능 구현
    기본 프로젝트 생성, 에러코드 기능 구현

---
+++ yoseobara/src/main/java/com/final2/yoseobara/YoseobaraApplication.java
@@ -0,0 +1,13 @@
+package com.final2.yoseobara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoseobaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoseobaraApplication.class, args);
+    }
+
+}
