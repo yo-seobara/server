e9e9c1a40ae3512bd2530eddf2ed4442c2bd5e88
siwonkim <108779402+ktldnjs@users.noreply.github.com>
2022-10-01 00:54:47 +0900

    fix: redirect_uri 수정, 테스트용코드 삭제 (#65)
    - 프론트랑 연동위해서 redirect_uri를 localhost:3000으로 변경
    - 소셜로그인 테스트용 코드 삭제
    
    Resolves: #45
    
    Co-authored-by: ktldnjs <ktldnjs0811@gmail.com>

--- yoseobara/src/main/java/com/final2/yoseobara/service/KakaoMemberService.java
+++ yoseobara/src/main/java/com/final2/yoseobara/service/KakaoMemberService.java
@@ -66,7 +66,7 @@ public class KakaoMemberService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", "e293cafd847d24faa5079a9e55814232");
-        body.add("redirect_uri", "http://localhost:8080/member/kakao/callback");
+        body.add("redirect_uri", "http://localhost:3000/member/kakao/callback");
         body.add("code", code);
 
         // HTTP 요청 보내기
